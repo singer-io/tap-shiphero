7280a435eff93a23b57e985913100987868377fe
Andy <luandy64@gmail.com>
2019-04-17 16:38:16 -0400

    Add the simplejson error to the backoff list, log some info when we h… (#11)
    * Add the simplejson error to the backoff list, log some info when we hit the error and retry
    
    * Do the thing

--- tap_shiphero/client.py
+++ tap_shiphero/client.py
@@ -2,9 +2,12 @@ from datetime import datetime, timedelta
 
 import backoff
 import requests
+import singer
 from requests import exceptions
 from singer import metrics
 from ratelimit import limits, RateLimitException, sleep_and_retry
+import simplejson
+LOGGER = singer.get_logger()
 
 class Server5xxError(Exception):
     pass
@@ -28,7 +31,8 @@ class ShipHeroClient(object):
                            RateLimitException,
                            exceptions.Timeout,
                            exceptions.ConnectionError,
-                           exceptions.ChunkedEncodingError),
+                           exceptions.ChunkedEncodingError,
+                           simplejson.scanner.JSONDecodeError),
                           max_tries=5,
                           factor=2)
     @sleep_and_retry
@@ -63,7 +67,15 @@ class ShipHeroClient(object):
 
         response.raise_for_status()
 
-        return response.json()
+        try:
+            return response.json()
+        except:
+            LOGGER.info('response.headers["content-type"]: ', response.headers['content-type'])
+
+            # This raise lets the backoff retry to kick in
+            raise
+
+
 
     def get(self, path, **kwargs):
         return self.request('GET', path, **kwargs)

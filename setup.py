ab2cf1cdcc9715ad73796053a677d40d94094b97
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='tap-shiphero',
-    version='1.1.9',
+    version='1.1.10',
     description="Singer.io tap for extracting Shiphero data",
     author="Stitch",
     url="http://singer.io",
